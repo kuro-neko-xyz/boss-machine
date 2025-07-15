4cff3c22145501d96a199fa5b409362d52367281
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-07-14 20:02:40 -0600

    Update Dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,30 @@
-'use strict';
+"use strict";
 
-const webpack = require('webpack'); // eslint-disable-line no-unused-vars
+const webpack = require("webpack"); // eslint-disable-line no-unused-vars
 
 module.exports = {
-  entry: './browser/index.js',
+  entry: "./browser/index.js",
   output: {
     path: __dirname,
-    filename: './public/js/bundle.js',
+    filename: "./public/js/bundle.js",
   },
   context: __dirname,
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
-    loaders: [
+    rules: [
       {
         test: /jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-2'],
-        }
-      }
-    ]
-  }
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-react",
+              "@babel/preset-env",
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
